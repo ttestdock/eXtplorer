09980712b4485e5f49364aca239569cf5313da29
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-12-04 00:30:39 +0700

    initial commit

---
+++ config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array("admin","0192023a7bbd73250516f069df18b500","/home/rully/public_html","http://localhost",1,"",7,1),
+); 
+?>
